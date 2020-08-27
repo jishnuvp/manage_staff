67be8786258aa21a2218239f913cfda6b9321466
jishnu <jishnuvpxyz@gmail.com>
2020-08-27 11:57:57 +0530

    chnaged interface into class and implemented a default constructor

--- angular-staff-manager/src/app/staff.ts
+++ angular-staff-manager/src/app/staff.ts
@@ -1,11 +1,12 @@
-export interface Staff {
-    Id: number,
-    Name: string,
-    StaffType: string,
-    EmpCode: string,
-    ContactNumber: string,
-    DateOfJoin: Date,
-    Subject: string,
-    Role: string,
-    Department: string
+export class Staff {
+    Id: number;
+    Name: string;
+    StaffType: string;
+    EmpCode: string;
+    ContactNumber: string;
+    DateOfJoin: Date;
+    Subject: string;
+    Role: string;
+    Department: string;
+    constructor() { }
 }
